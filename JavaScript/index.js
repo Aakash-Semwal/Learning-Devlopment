8727ea3f3df5221c256e2bb7ab524300d6a4bd78
Aakash Semwal <semwalaakash24@gmail.com>
2025-06-11 12:33:41 +0530

    Learn Event Loop

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,34 @@
-let element = document.querySelector('#wrapper');
+// const t1 = performance.now();
+//
+// for( let i = 1 ; i <= 100 ; i++){
+//     let newElement = document.createElement("p");
+//     newElement.textContent = "This is para "+i;
+//     document.body.appendChild(newElement);
+// }
+//
+// const t2 = performance.now();
+// console.log("Time taken 1 = " + (t2 - t1));
 
-element.addEventListener('click', function(event){
-    if(event.target.nodeName === 'SPAN')
-        console.log("Span is clicked " + event.target.textContent);
-})
+// const t3 = performance.now();
+// let myDiv = document.createElement("div");
+// for( let i = 1 ; i <= 100 ; i++){
+//     let element = document.createElement("p");
+//     element.textContent = "This is para "+i;
+//     myDiv.appendChild(element);
+// }
+// document.body.appendChild(myDiv);
+// const t4 = performance.now();
+// console.log("Time taken 2 = " + (t4 - t3));
+
+const t1 = performance.now();
+
+let fragment = document.createDocumentFragment();
+
+for( let i = 1 ; i <= 100 ; i++){
+    let newElement = document.createElement("p");
+    newElement.textContent = "This is para "+i;
+    fragment.appendChild(newElement);
+}
+document.body.appendChild(fragment);
+const t2 = performance.now();
+console.log("Time taken 1 = " + (t2 - t1));
